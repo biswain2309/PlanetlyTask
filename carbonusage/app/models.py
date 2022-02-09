040c0e384be77bbf67125642a7c473c60a7ae5c0
Indrani Biswas <indranibiswas@surfer-172-30-10-168-hotspot.internet-for-guests.com>
2022-02-09 16:49:11 +0100

    Initial commit

---
+++ carbonusage/app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+
+# class User(models.Model):
+#     '''User model defines the attributes of an User'''
+#     id = models.BigIntegerField()
+#     name = models.CharField(max_length=255)
+
+
+
